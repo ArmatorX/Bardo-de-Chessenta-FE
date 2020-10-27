db3f401953f99f24384ad01d562db1d4d27d4f07
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2020-10-27 17:46:31 -0300

    Reproducción funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import * as Feather from 'feather-icons';
+import { Cancion, CancionService } from './services/cancion.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Bardo de Chessenta';
+export class AppComponent implements OnInit {
+    canciones : Cancion[] = [];
+    title = 'Bardo de Chessenta';
 
-  ngAfterViewInit() {
-    Feather.replace();
-  }
+    constructor (private servicio : CancionService, private router : Router, private route: ActivatedRoute) { }
+
+    ngOnInit(): void { 
+        this.getCanciones(0);
+    }
+
+    ngAfterViewChecked() {
+        Feather.replace();
+    }
+
+    getCanciones(nroPagina : number) {
+        let params = new HttpParams();
+
+        params = params.append('page', nroPagina.toString());
+        
+        this.servicio.get(params).subscribe(respuesta => {
+            this.canciones = respuesta.content;
+        });
+    }
+
+    reproducir(cancionId : number) {
+        this.servicio.reproducir(cancionId).subscribe();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/templates/navbar/navbar.component';
 import { MenuComponent } from './components/templates/menu/menu.component';
+import { CancionService } from './services/cancion.service';
+import { EmocionEspecificaService } from './services/emocion-especifica.service';
+import { LugarService } from './services/lugar.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,10 @@ import { MenuComponent } from './components/templates/menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CancionService, EmocionEspecificaService, LugarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
